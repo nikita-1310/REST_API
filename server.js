619df12d3f9f96695b767e1e0838a95473464fec
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-05-22 00:04:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const connectDB = require('./db/conn')
+require("dotenv").config()
+
+const product_route = require("./routes/products")
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send("hiii")
+})
+
+//middelware or set to router   
+app.use("/api/products", product_route)
+
+const start= async()=>{
+    try{
+        await connectDB(process.env.MONGODB_URL)
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port no ${PORT}`);
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+start();
